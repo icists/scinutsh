ba6f1acd0c1d946945701cd4ffb45a6f207f0783
Gangtae Park <rkdxo0417@gmail.com>
2019-06-22 18:46:38 +0900

    Create Topic Card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import TopicCard from '../../components/TopicCard/index';
+
 const Home: React.FC = () => (
   <div className='home-page'>
     <h1>
@@ -14,13 +16,13 @@ const Home: React.FC = () => (
       This section is for topic cards.
       <ul>
         <li>
-          <Link to='topic/1'>Topic 1</Link>
+          <Link to='topic/1'><TopicCard topicNum='1' topic='Topic 1' description='blabla'></TopicCard></Link>
         </li>
         <li>
-          <Link to='topic/2'>Topic 2</Link>
+          <Link to='topic/2'><TopicCard topicNum='2' topic='Topic 2' description='blabla'></TopicCard></Link>
         </li>
         <li>
-          <Link to='topic/3'>Topic 3</Link>
+          <Link to='topic/3'><TopicCard topicNum='3' topic='Topic 3' description='blabla'></TopicCard></Link>
         </li>
       </ul>
     </div>
